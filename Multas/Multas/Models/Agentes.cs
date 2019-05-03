2cf17478180da9a8c576283622a8deb7dfa07e38
Virgilio Quintino <estt8786@ipt.pt>
2019-05-03 19:26:26 +0100

    Adição de anotadores à classe dos Agentes, para validar o tipo de dados intriduzidos

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -15,19 +15,28 @@ namespace Multas.Models {
         /// - foto
         // Agentes
 
+        // para forçar que é chave primaria PK
+        [Key]
         public int ID { get; set; }
 
         // ******************** CONTROLO DO CAMPO NOME *********************
-        [Required(ErrorMessage = "Por favor, escreva o Nome do Agente!")]
-        [RegularExpression("([A-ZÁÉÍÓÚa-záéíóúàèìòùäëïöüãõâêîôûçñ]+( |-|')?)+", ErrorMessage = "Só pode escrever letras no nome. Deve começar por uma maiúscula!")]
+        // por defeito, se ficar assim aparecem as mensagens default do sistema!
+        //[Required]
+        [Required(ErrorMessage = "Por favor, escreva o Nome do Agente! É de preenchimento obrigatório!")]
+        [StringLength(40)]
+        [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùâêîôûçñ]+(( (e|(d(e|a|o)(s)?) )? |-|')[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùâêîôûçñ]+){1,3}", 
+            ErrorMessage = "Só pode escrever letras no nome. Deve começar por uma maiúscula!")]
         public string Nome { get; set; }
 
         // ********************* CONTROLO DO CAMPO ESQUADRA **************************
-        [Required(ErrorMessage = "Não se esqueça de indicar a Esquadra onde o agente trabalha, por favor!")]
-        //   [RegularExpression("(Tomar|Ourém|Torres Novas|Lisboa|Leiria)")]
-
+        [Required(ErrorMessage = "Não se esqueça de indicar a Esquadra onde o Agente trabalha, por favor! É de preenchimento obrigatório!")]
+        [RegularExpression("(Tomar|Ourém|Torres Novas|Lisboa|Leiria)", 
+            ErrorMessage = "Só aceita as esquadras Tomar, Ourém, Torres Novas, Lisboa ou Leiria")]
+        [StringLength(30)]
         public string Esquadra { get; set; }
 
+        // ********************* CONTROLO DO CAMPO FOTOGRAFIA **************************
+        [StringLength(30)]
         public string Fotografia { get; set; }
 
         // lista das multas associadas ou passadas pelo Agente
